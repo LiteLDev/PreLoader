1948c2d819ba696ceaa0a85f534609b5104c00ee
RimuruChan <RealSprite233@outlook.com>
2023-03-19 21:35:51 +0800

    ci: try add build cache

--- xmake.lua
+++ xmake.lua
@@ -8,10 +8,10 @@ else
     set_runtimes("MD")
 end
 
-add_requires("fmt")
-add_requires("raw_pdb")
-add_requires("nlohmann_json")
-add_requires("parallel-hashmap")
+add_requires("fmt 9.1.0")
+add_requires("raw_pdb 2022.10.17")
+add_requires("nlohmann_json v3.11.2")
+add_requires("parallel-hashmap 1.35")
 
 add_requires("detours v4.0.1-xmake.0")
 add_requires("demangler v1.0.1")
@@ -23,6 +23,6 @@ target("PreLoader")
     add_headerfiles("include/**.h")
     add_includedirs("./include")
     add_defines("PRELOADER_EXPORT", "UNICODE")
-    add_cxxflags("/UTF-8")
+    add_cxflags("/utf-8")
     add_files("src/**.cpp")
     add_packages("raw_pdb", "nlohmann_json", "parallel-hashmap", "demangler", "detours", "fmt")
