7cd341ef68f398e6f0dc0e015500d9d4314d6719
RimuruChan <RealSprite233@outlook.com>
2023-03-22 22:20:20 +0800

    build: fix runtime option

--- xmake.lua
+++ xmake.lua
@@ -2,10 +2,12 @@ add_rules("mode.debug", "mode.release")
 
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 
-if is_mode("debug") then
-    set_runtimes("MDd")
-else
-    set_runtimes("MD")
+if not get_config("vs_runtime") then
+    if is_mode("debug") then
+        set_runtimes("MDd")
+    else
+        set_runtimes("MD")
+    end
 end
 
 add_requires("fmt 9.1.0")
@@ -13,8 +15,8 @@ add_requires("raw_pdb 2022.10.17")
 add_requires("nlohmann_json v3.11.2")
 add_requires("parallel-hashmap 1.35")
 
-add_requires("detours v4.0.1-xmake.0")
-add_requires("demangler v1.0.1")
+add_requires("detours v4.0.1-xmake.1")
+add_requires("demangler v1.0.2")
 
 target("PreLoader")
     set_kind("shared")
