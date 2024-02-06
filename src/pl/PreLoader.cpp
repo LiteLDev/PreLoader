537a2a739a2ac50fc3ffa9a0d151ef2ec30a9187
dofes <91889957+dofes@users.noreply.github.com>
2024-02-06 14:31:53 +0800

    refactor: refactor preload plugin loading in PreLoader.cpp

--- src/pl/PreLoader.cpp
+++ src/pl/PreLoader.cpp
@@ -91,9 +91,9 @@ void loadRawLibraries() {
 }
 
 void loadPreloadNativePlugins() {
-    namespace fs = std::filesystem;
-    std::filesystem::create_directories("plugins");
+    namespace fs        = std::filesystem;
     fs::path pluginsDir = ".\\plugins";
+    std::filesystem::create_directories(pluginsDir);
     for (const auto& entry : fs::directory_iterator(pluginsDir)) {
         if (!entry.is_directory()) { continue; }
         fs::path manifestPath = entry.path() / "manifest.json";
